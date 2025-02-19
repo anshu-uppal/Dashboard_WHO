acf5f4730151c0b18dfdb9586a42ca934ed7753d
anshu-uppal <anshu.uppal87@gmail.com>
2025-02-19 12:24:04 +0100

    Added code for including a table of filtered data

--- code/utils.R
+++ code/utils.R
@@ -1,3 +1,20 @@
+#' A custom formatter using flextable
+#'
+#' @param data Table to make into flextable object
+#' @returns A custom flextable output
+#' @export
+#'
+#' @examples
+#' myflextable(mtcars)
+myflextable = function(data) {
+        flextable::set_flextable_defaults(na_str = "NA", font.size = 12, padding.bottom = 1, padding.top = 1)
+        x = flextable::flextable(data)
+        x = flextable::colformat_int(x, big.mark = "")
+        x = flextable::colformat_double(x, big.mark = "", digits = 2, na_str = "NA")
+        x = flextable::theme_booktabs(x)
+        return(x)
+}
+
 #' Generate individual country estimates over time for the indicator, setting, and dimension of choice.
 #'
 #' @param dataset Choose the dataset
@@ -54,4 +71,53 @@ make_lineplot <- function(
                 facet_wrap(.~setting, labeller = label_wrap_gen())+
                 theme_classic()
         p
+}
+
+#' Generate a table based on the filtered dataset
+#'
+#' @param dataset 
+#' @param caption_indicator 
+#' @param caption_dimension 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+make_table <- function(
+                dataset, 
+                caption_indicator = "indicator",
+                caption_dimension = "dimension"
+){
+        a<-dataset |> 
+                # mutate(indicator_name = str_remove(indicator_name, " \\(\\%\\)")) |> 
+                arrange(setting, year, indicator_name, subgroup, subgroup_order) |> 
+                select(setting, year, subgroup, full_estimate, population)
+        
+        # Automatically get linenumbers to visually break up the table
+        b <- a |> 
+                group_by(setting, year) |>
+                mutate(
+                        setting = ifelse(!duplicated(setting), setting, ""),
+                        year = ifelse(!duplicated(year), year, "")
+                ) |>
+                ungroup() |> 
+                mutate(lineref = case_when(setting=="" ~ FALSE, .default = TRUE)) |> 
+                select(lineref) |> 
+                rowid_to_column() |> 
+                filter(lineref)
+        groupnames <- setdiff(b$rowid-1, 0) # remove 0, first line not necessary
+        
+        table_output <- myflextable(a) |> 
+                set_header_labels(values = c("Setting", "Year", "Subgroup", 
+                                             "Estimate [95% CI]*", "Population")) |> 
+                hline(i = groupnames, border = fp_border_default(color = "black")) |>
+                autofit() |> 
+                add_footer_lines(c(
+                        paste0("Dimension: ", caption_dimension),
+                        paste0("Source: ", "Health Inequality Data Repository (WHO)")
+                )) |>
+                set_caption(caption = paste0(caption_indicator),
+                            align_with_table = FALSE)
+        
+        return(table_output)
 }
