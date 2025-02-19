f3721c581e49d1ab7cdca0b0d29fdf0fbe6f5f7f
anshu-uppal <anshu.uppal87@gmail.com>
2025-02-19 15:26:25 +0100

    remove extra datasets

--- code/01_prep_data.R
+++ code/01_prep_data.R
@@ -9,9 +9,3 @@ pacman::p_load(
 # Convert downloaded xlsx files to RDS format for easier downstream use
 rep_imm <- openxlsx::read.xlsx(here::here("data", "rep_imm", "data.xlsx"))
 saveRDS(rep_imm, here::here("data", "rep_imm", "rep_imm.rds"))
-
-rep_dtp_sub <- openxlsx::read.xlsx(here::here("data", "rep_dtp_sub", "data.xlsx"))
-saveRDS(rep_dtp_sub, here::here("data", "rep_dtp_sub", "rep_dtp_sub.rds"))
-
-rep_unaids_hiv <- openxlsx::read.xlsx(here::here("data", "rep_unaids_hiv", "data.xlsx"))
-saveRDS(rep_unaids_hiv, here::here("data", "rep_unaids_hiv", "rep_unaids_hiv.rds"))
