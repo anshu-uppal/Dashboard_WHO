7d8ac3166bebea41f938cd4c73eaefb75710e66d
anshu-uppal <anshu.uppal87@gmail.com>
2025-03-03 12:03:36 +0100

    added harmonized name variable

--- code/02_settings_map_data.R
+++ code/02_settings_map_data.R
@@ -13,7 +13,6 @@ rep_comb <- readRDS(here::here("data", "combined_dataset.rds"))
 # Get world map data
 world <- rnaturalearth::ne_countries(scale = "large", returnclass = "sf")
 world_filtered <- world |> filter(name_long != "Antarctica") |> 
-        relocate(admin, name_long, iso_a3, iso_a3_eh) |>
         select(admin, name_long, iso_a3,iso_a3_eh, formal_en, 
                pop_est:income_grp)
 
@@ -22,6 +21,10 @@ settings <- rep_comb |> select(setting, iso3) |> distinct()
 
 world_final <- world_filtered |> 
         left_join(settings, by = join_by(iso_a3_eh == iso3)) |> 
-        relocate(setting)
+        mutate(admin2 = case_when(
+                is.na(setting) ~ admin,
+                .default = setting
+        )) |> 
+        relocate(setting, admin, admin2)
 
 saveRDS(world_final, here::here("data", "world_settings_matched.rds"))
