70f2226d75a9887f662599478f2e5df5f809337a
anshu-uppal <anshu.uppal87@gmail.com>
2025-03-03 11:23:22 +0100

    Use ISO 3166-1 alpha-3 codes for country matching

--- code/02_settings_map_data.R
+++ code/02_settings_map_data.R
@@ -13,69 +13,15 @@ rep_comb <- readRDS(here::here("data", "combined_dataset.rds"))
 # Get world map data
 world <- rnaturalearth::ne_countries(scale = "large", returnclass = "sf")
 world_filtered <- world |> filter(name_long != "Antarctica") |> 
-        relocate(admin, name_long) |> 
-        mutate(
-                admin = case_match(
-                        admin,
-                        "Bolivia" ~ "Bolivia (Plurinational State of)",
-                        "Republic of the Congo" ~ "Congo",
-                        "North Korea" ~ "Democratic People's Republic of Korea",
-                        "eSwatini" ~ "Eswatini",
-                        "Iran" ~ "Iran (Islamic Republic of)",
-                        "Laos" ~ "Lao People's Democratic Republic",
-                        "Federated States of Micronesia" ~ "Micronesia (Federated States of)",
-                        "Netherlands" ~ "Netherlands (Kingdom of the)",
-                        "Moldova" ~ "Republic of Moldova",
-                        "São Tomé and Principe" ~ "Sao Tome and Principe",
-                        "Syria" ~ "Syrian Arab Republic",
-                        "United Kingdom" ~ "The United Kingdom",
-                        # ~ "Tokelau",
-                        "Turkey" ~ "Türkiye",
-                        "Venezuela" ~ "Venezuela (Bolivarian Republic of)",
-                        "Vietnam" ~ "Viet Nam",
-                        "Palestine" ~ "occupied Palestinian territory",
-                        .default = admin
-                ))
+        relocate(admin, name_long, iso_a3, iso_a3_eh) |>
+        select(admin, name_long, iso_a3,iso_a3_eh, formal_en, 
+               pop_est:income_grp)
 
 settings <- tibble(setting = unique(rep_comb$setting)) |> droplevels()
-
-world_settings <- 
-        fuzzyjoin::stringdist_left_join(
-                settings, world_filtered,
-                by = c(setting = "admin"),
-                distance_col = "dist"
-        ) |> 
-        group_by(setting) |>
-        slice_min(order_by=dist, n=1) |>
-        relocate(dist, .after = admin) |> 
-        ungroup()
-
-world_settings_b <- world_settings |> 
-        filter(is.na(dist) | dist != 0) |> 
-        select(setting) |> 
-        fuzzyjoin::stringdist_left_join(
-                world_filtered,
-                by = c(setting = "name_long"),
-                distance_col = "dist"
-        ) |> 
-        group_by(setting) |>
-        slice_min(order_by=dist, n=1) |>
-        relocate(dist, .after = admin) |> 
-        ungroup()
-
-settings_remaining <- world_settings_b |> 
-        filter(is.na(dist) | dist != 0) |> 
-        select(setting) |> 
-        distinct()
-
-world_settings_matched <- 
-        world_settings |> 
-        filter(dist ==0) |> 
-        bind_rows(world_settings_b |> filter(dist == 0))
+settings <- rep_comb |> select(setting, iso3) |> distinct()
 
 world_final <- world_filtered |> 
-        left_join(world_settings_matched |> select(setting, admin)) |> 
-        st_as_sf() |> 
-        select(setting, admin)
+        left_join(settings, by = join_by(iso_a3_eh == iso3)) |> 
+        relocate(setting)
 
 saveRDS(world_final, here::here("data", "world_settings_matched.rds"))
